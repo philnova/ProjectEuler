2e38462f12eae5c1004ad31157202ac379ade023
Phil Nova <pnova8@gmail.com>
2015-07-12 10:41:50 -0700

    Preallocating cache leads to huge performance increase!

--- euler11_20.py
+++ euler11_20.py
@@ -396,32 +396,30 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
-COLLATZ_CACHE = {2 : 2, 1 : 1}
+COLLATZ_CACHE = {i:0 for i in xrange(1,1000000)}
+COLLATZ_CACHE[1] = 1
 def collatz_length(n, limit = 1000000):
 	modifier = 0
 	original_val = n
-	if n in COLLATZ_CACHE.keys():
-		return COLLATZ_CACHE[n]
-	else:
-		collatz_list = [n]
-		while not n in COLLATZ_CACHE.keys():
-			
-			if n%2 == 0:
-				n = n/2
-			else:
-				n = (3*n)+1
-			collatz_list.append(n)
-		modifier = COLLATZ_CACHE[n]
-		#print collatz_list
-		while collatz_list:
-			latest = collatz_list.pop(0)
-			if latest in COLLATZ_CACHE.keys():
-				break
-			elif latest < limit:
-				COLLATZ_CACHE[latest] = len(collatz_list)+modifier
+
+
+	collatz_list = [n]
+	while n > limit-1 or COLLATZ_CACHE[n] == 0:
+		if n%2 == 0:
+			n = n/2
+		else:
+			n = (3*n)+1
+		collatz_list.append(n)
+	modifier = COLLATZ_CACHE[n]
+	#print collatz_list
+	while collatz_list:
+		latest = collatz_list.pop(0)
+		if latest in COLLATZ_CACHE.keys():
+			break
+		elif latest < limit:
+			COLLATZ_CACHE[latest] = len(collatz_list)+modifier
 	return COLLATZ_CACHE[original_val]
 
-print collatz_length(3)
 
 def find_longest_collatz_chain(limit=1000000):
 	longest_chain = 0
@@ -442,19 +440,16 @@ def find_longest_collatz_chain(limit=1000000):
 # previous solution works but has PyPy runtime of ~12hours. Needs serious optimization
 
 def collatz_length_2(n, limit = 1000000):
-	if n in COLLATZ_CACHE.keys():
-		return COLLATZ_CACHE[n]
-	else:
-		original_val = n
-		counter = 0
-		while not n in COLLATZ_CACHE.keys():
-			if n % 2 == 0:
-				n = n/2
-			else:
-				n = (3*n)+1
-			counter += 1
-		COLLATZ_CACHE[original_val] = COLLATZ_CACHE[n] + counter
-		return COLLATZ_CACHE[original_val]
+	original_val = n
+	counter = 0
+	while n > limit-1 or COLLATZ_CACHE[n] == 0:
+		if n % 2 == 0:
+			n = n/2
+		else:
+			n = (3*n)+1
+		counter += 1
+	COLLATZ_CACHE[original_val] = COLLATZ_CACHE[n] + counter
+	return COLLATZ_CACHE[original_val]
 
 def find_longest_collatz_chain_2(limit=1000000):
 	longest_chain = 0
@@ -469,9 +464,65 @@ def find_longest_collatz_chain_2(limit=1000000):
 	return generating_integer, longest_chain
 
 start1 = time.time()
-print find_longest_collatz_chain_2(10000)
+print find_longest_collatz_chain_2()
 print time.time()-start1
 
-start2 = time.time()
-print find_longest_collatz_chain(10000)
-print time.time()-start2
+#start2 = time.time()
+#print find_longest_collatz_chain(10000)
+#print time.time()-start2
+
+# our second algorithm gives a ~6x speedup
+# let's try ditching the cache
+
+start = time.time()
+ 
+def collatz(n, count=1):
+    while n > 1:
+        count += 1
+        if n % 2 == 0:
+            n = n/2
+        else:
+            n = 3*n + 1
+    return count
+ 
+max = [0,0]
+for i in range(1000000):
+    c = collatz(i)
+    if c > max[0]:
+        max[0] = c
+        max[1] = i
+ 
+elapsed = (time.time() - start)
+print "found %s at length %s in %s seconds" % (max[1],max[0],elapsed)
+
+# the entire process completes in 51 seconds with no cache!
+
+# now let's add a cache
+start = time.time()
+ 
+limit = 1000000
+collatz_length = [0] * limit
+collatz_length[1] = 1
+max_length = [1,1]
+ 
+for i in range(1,1000000):
+    n,s = i,0
+    TO_ADD = [] # collatz_length not yet known
+    while n > limit - 1 or collatz_length[n] < 1: 
+        TO_ADD.append(n)
+        if n % 2 == 0: n = n/2
+        else: n = 3*n + 1
+        s += 1
+    # collatz_length now known from previous calculations
+    p = collatz_length[n]
+    for j in range(s):
+        m = TO_ADD[j]
+        if m < limit:
+            new_length = collatz_length[n] + s - j
+            collatz_length[m] = new_length
+            if new_length > max_length[1]: max_length = [i,new_length]
+ 
+elapsed = (time.time() - start)
+print "found %s at length %s in %s seconds" % (max_length[0],max_length[1],elapsed)
+
+# with this approach, our solution is down to 5 s!
