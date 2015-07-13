a8d5805afc0c5fa1997764fb77a0c556006d3cf9
Phil Nova <pnova8@gmail.com>
2015-07-12 23:07:37 -0700

    First attempt at C++ caching

--- collatz/collatz/main.cpp
+++ collatz/collatz/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <vector>
 using namespace std;
 
 int collatz_naive(unsigned long val) {
@@ -41,19 +42,30 @@ pair<int,int> max_collatz_naive(int limit) {
     return make_pair(max_chain, max_collatz);
 }
 
-int collatz_array[1];
+vector <int> collatz_array(1000000,0);
 
 int collatz_cached(unsigned long val, int limit) {
+    if (collatz_array[val]!=0){
+        return collatz_array[val];
+    }
     int counter = 0;
-    int original_val = val;
     
+    vector<unsigned long> DynArrNums;
     while (val > limit - 1 or collatz_array[val] == 0){
-        if (val % 2 == 0)
+        if (val % 2 == 0){
             val = val / 2;
+        }
         else
             val = (3 * val) + 1;
         counter ++;
-        //need to add result to a dynamic array
+        DynArrNums.push_back(val); //look out for type coersion here
+        
+    }
+    unsigned long modifier = collatz_array[val];
+    for (int i=0;i<DynArrNums.size();i++){
+        if (DynArrNums[i] < limit){
+            collatz_array[DynArrNums[i]] = modifier + DynArrNums.size() - i;
+        }
     }
     
     return 0;
@@ -64,7 +76,7 @@ pair<int,int> max_collatz_cached(int limit) {
     int max_collatz = 0;
     int current_chain = 0;
     
-    int collatz_array[limit];
+    //vector<int> collatz_array(limit,0);
     collatz_array[1] = 1;
     
     for (int i=1; i<limit; i++){
@@ -75,8 +87,6 @@ pair<int,int> max_collatz_cached(int limit) {
         }
     }
 
-    
-    
     return make_pair(max_chain, max_collatz);
 }
 
@@ -85,8 +95,13 @@ int main(int argc, const char * argv[]) {
     //int output = collatz_naive(113383);
     //cout << output;
     double starttime;
-    //starttime = clock();
+    starttime = clock();
     pair<int,int>answer = max_collatz_naive(1000000);
     cout << "Max chain length = " << answer.first << " at integer = " << answer.second << endl << " in " << (clock() - starttime)/CLOCKS_PER_SEC << endl;
+    
+    starttime = clock();
+    answer = max_collatz_cached(1000000);
+    cout << "Max chain length = " << answer.first << " at integer = " << answer.second << endl << " in " << (clock() - starttime)/CLOCKS_PER_SEC << endl;
+    
     return 0;
 }
