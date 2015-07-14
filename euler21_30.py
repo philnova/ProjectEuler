5d30792f96474d95030b7c7fc7b40a526c07a2a8
Phil Nova <pnova8@gmail.com>
2015-07-14 15:02:43 -0700

    Solved p. 22

--- euler21_30.py
+++ euler21_30.py
@@ -1,5 +1,6 @@
 import math
 import time
+import string
 
 #======================================#
 
@@ -34,4 +35,37 @@ def amicables(limit=10000):
 				amicable_list.append(current)
 	return sum(list(set(amicable_list)))
 
-print amicables()
+#print amicables()
+
+#======================================#
+
+"""Problem 22:
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
+
+alphlist = list(string.ascii_uppercase)
+ALPHADICT = {letter : idx+1 for idx, letter in enumerate(alphlist)}
+
+def namescore(name):
+	return sum([ALPHADICT[letter] for letter in name])
+
+total = 0
+with open("p022_names.txt","r") as fo:
+	for line in fo:
+		names_list = line.split(',')
+
+cleaned_list = []
+for name in names_list:
+	cleaned_list.append(name[1:-1])
+cleaned_list.sort()
+
+
+total = 0
+for idx, name in enumerate(cleaned_list):
+	total += (idx+1) * namescore(name)
+
+print total
