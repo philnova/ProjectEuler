0858191598ba570688d12881ed34e2ac8d766b01
Phil Nova <pnova8@gmail.com>
2015-07-22 18:11:16 -0700

    Solved p. 43 with brute force

--- euler41_50.py
+++ euler41_50.py
@@ -90,6 +90,53 @@ def count_triangle_words(wordfile):
 	return counter
 
 
-print count_triangle_words("words42.txt")
+#print count_triangle_words("words42.txt")
 
+#======================================#
 
+"""
+Problem 43:
+
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+d2d3d4=406 is divisible by 2
+d3d4d5=063 is divisible by 3
+d4d5d6=635 is divisible by 5
+d5d6d7=357 is divisible by 7
+d6d7d8=572 is divisible by 11
+d7d8d9=728 is divisible by 13
+d8d9d10=289 is divisible by 17
+Find the sum of all 0 to 9 pandigital numbers with this property.
+
+"""
+
+DIGITS = "1 2 3 4 5 6 7 8 9 0".split()
+
+def gen_pandigitals(n_digits):
+	return [i for i in reversed(sorted(["".join(seq) for seq in itertools.permutations(DIGITS[0:n_digits])]))]
+
+def check_property(digit_string):
+	if int(digit_string[1:4])%2==0:
+		if int(digit_string[2:5])%3==0:
+			if int(digit_string[3:6])%5==0:
+				if int(digit_string[4:7])%7==0:
+					if int(digit_string[5:8])%11==0:
+						if int(digit_string[6:9])%13==0:
+							if int(digit_string[7:10])%17==0:
+								return int(digit_string)
+	return 0
+
+#print check_property('1406357289')
+#print check_property('1406357288')
+
+def find_sum_of_pandigital_with_property():
+	total = 0
+	pandigitals = gen_pandigitals(10)
+	for pan in pandigitals:
+		print pan
+		total += check_property(pan)
+	return total
+
+print find_sum_of_pandigital_with_property()
